f3bc83d2eb978cbab7c9325ba41ce5f96fc09ed7
KeplerBoyce <keplerboy@gmail.com>
2022-09-03 20:58:44 -0700

    camera damping

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 use bevy::render::camera::ScalingMode;
 use bevy_inspector_egui::WorldInspectorPlugin;
+use num::clamp;
 
 fn main() {
     App::new()
@@ -54,29 +55,81 @@ fn setup(
         .into(),
         transform: Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
+    }).insert(PanCamera {
+        ..Default::default()
     });
 }
 
+#[derive(Component)]
+struct PanCamera {
+    pub max_speed: f32,
+    pub accel: f32,
+    pub speed_x: f32,
+    pub speed_z: f32,
+}
+
+impl Default for PanCamera {
+    fn default() -> Self {
+        PanCamera {
+            max_speed: 3.0,
+            accel: 10.0,
+            speed_x: 0.0,
+            speed_z: 0.0,
+        }
+    }
+}
+
+//move camera with WASD keys
 fn move_camera(
     keys: Res<Input<KeyCode>>,
-    mut camera: Query<&mut Transform, With<Camera>>,
+    time: Res<Time>,
+    mut query: Query<(&mut PanCamera, &mut Transform), With<PanCamera>>,
 ) {
-    for mut transform in camera.iter_mut() {
+    for (mut pan_camera, mut transform) in query.iter_mut() {
+        //set target speeds based on keys pressed
+        let mut target_speed_x = 0.0;
+        let mut target_speed_z = 0.0;
         if keys.pressed(KeyCode::W) {
-            transform.translation.x -= 0.1;
-            transform.translation.z -= 0.1;
+            target_speed_x -= pan_camera.max_speed;
+            target_speed_z -= pan_camera.max_speed;
         }
         if keys.pressed(KeyCode::A) {
-            transform.translation.x -= 0.1;
-            transform.translation.z += 0.1;
+            target_speed_x -= pan_camera.max_speed;
+            target_speed_z += pan_camera.max_speed;
         }
         if keys.pressed(KeyCode::S) {
-            transform.translation.x += 0.1;
-            transform.translation.z += 0.1;
+            target_speed_x += pan_camera.max_speed;
+            target_speed_z += pan_camera.max_speed;
         }
         if keys.pressed(KeyCode::D) {
-            transform.translation.x += 0.1;
-            transform.translation.z -= 0.1;
+            target_speed_x += pan_camera.max_speed;
+            target_speed_z -= pan_camera.max_speed;
         }
+        //adjust diagonal movement speed
+        if target_speed_x != 0.0 && target_speed_z != 0.0 {
+            target_speed_x /= (2 as f32).sqrt();
+            target_speed_z /= (2 as f32).sqrt();
+        }
+        //accelerate x speed to target
+        if (pan_camera.speed_x - target_speed_x).abs() < pan_camera.accel * time.delta_seconds() {
+            pan_camera.speed_x = target_speed_x;
+        } else if pan_camera.speed_x < target_speed_x {
+            pan_camera.speed_x += pan_camera.accel * time.delta_seconds();
+        } else {
+            pan_camera.speed_x -= pan_camera.accel * time.delta_seconds();
+        }
+        //accelerate z speed to target
+        if (pan_camera.speed_z - target_speed_z).abs() < pan_camera.accel * time.delta_seconds() {
+            pan_camera.speed_z = target_speed_z;
+        } else if pan_camera.speed_z < target_speed_z {
+            pan_camera.speed_z += pan_camera.accel * time.delta_seconds();
+        } else {
+            pan_camera.speed_z -= pan_camera.accel * time.delta_seconds();
+        }
+        //clamp speeds and apply translation
+        pan_camera.speed_x = clamp(pan_camera.speed_x, -pan_camera.max_speed, pan_camera.max_speed);
+        pan_camera.speed_z = clamp(pan_camera.speed_z, -pan_camera.max_speed, pan_camera.max_speed);
+        transform.translation.x += pan_camera.speed_x * time.delta_seconds();
+        transform.translation.z += pan_camera.speed_z * time.delta_seconds();
     }
 }
