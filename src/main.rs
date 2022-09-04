99f5f80359834170fdd7e67e626b97883816ad2b
KeplerBoyce <keplerboy@gmail.com>
2022-09-03 19:16:26 -0700

    isometric view with camera movement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,82 @@
+use bevy::prelude::*;
+use bevy::render::camera::ScalingMode;
+use bevy_inspector_egui::WorldInspectorPlugin;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(WorldInspectorPlugin::new())
+        .add_startup_system(setup)
+        .add_system(move_camera)
+        .run();
+}
+
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane {
+            size: 5.0
+        })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+    // cube
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube {
+            size: 1.0
+        })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+    // light
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 5000.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    // camera
+    commands.spawn_bundle(Camera3dBundle {
+        projection: OrthographicProjection {
+            scale: 3.0,
+            scaling_mode: ScalingMode::FixedVertical(2.0),
+            ..default()
+        }
+        .into(),
+        transform: Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
+
+fn move_camera(
+    keys: Res<Input<KeyCode>>,
+    mut camera: Query<&mut Transform, With<Camera>>,
+) {
+    for mut transform in camera.iter_mut() {
+        if keys.pressed(KeyCode::W) {
+            transform.translation.x -= 0.1;
+            transform.translation.z -= 0.1;
+        }
+        if keys.pressed(KeyCode::A) {
+            transform.translation.x -= 0.1;
+            transform.translation.z += 0.1;
+        }
+        if keys.pressed(KeyCode::S) {
+            transform.translation.x += 0.1;
+            transform.translation.z += 0.1;
+        }
+        if keys.pressed(KeyCode::D) {
+            transform.translation.x += 0.1;
+            transform.translation.z -= 0.1;
+        }
+    }
 }
